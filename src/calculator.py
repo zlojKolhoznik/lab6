79d38b888054bd664343500868cc36f3e76d320a
Roman Portianko <rportyanko10@gmail.com>
2025-03-26 21:32:18 +0200

    Created calculator

---
+++ src/calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        return a / b
---
+++ src/main.py
@@ -0,0 +1,28 @@
+# Usage: calc <add|subtract|mult|div> <number1> <number2>
+# Example: calc add 2 3
+
+import sys
+from calculator import Calculator
+
+operations = {
+    "add": Calculator.add,
+    "subtract": Calculator.subtract,
+    "mult": Calculator.multiply,
+    "div": Calculator.divide
+}
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: calc <add|subtract|mult|div> <number1> <number2>")
+        sys.exit(1)
+
+    a = float(sys.argv[2])
+    b = float(sys.argv[3])
+    if operations.keys().__contains__(sys.argv[1]):
+        print(operations[sys.argv[1]](a, b))
+    else:
+        print("Invalid operation")
+    
+
+if __name__ == "__main__":
+    main()
